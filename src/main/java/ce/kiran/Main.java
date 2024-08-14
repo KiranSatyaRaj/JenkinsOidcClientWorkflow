dc677391f4158737d047b47665d745a2b0b2c048
KiranSatyaRaj <kiranjamy20021508@gmail.com>
2024-08-14 19:24:39 +0530

    first commit

---
+++ src/main/java/ce/kiran/Main.java
@@ -0,0 +1,12 @@
+package ce.kiran;
+
+import java.util.Map;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> env = System.getenv();
+        System.out.println(env.toString());
+    }
+}
