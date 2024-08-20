f64097054611846d10ee9e4c7ee0da31202fbb22
KiranSatyaRaj <kiranjamy20021508@gmail.com>
2024-08-21 03:45:08 +0530

    add JenkinsOidcClient and sign a file

--- src/main/java/ce/kiran/JenkinsOidcClient.java
+++ src/main/java/ce/kiran/JenkinsOidcClient.java
@@ -1,20 +1,108 @@
 package ce.kiran;
 
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.json.webtoken.JsonWebSignature;
+import dev.sigstore.http.HttpClients;
+import dev.sigstore.http.HttpParams;
+import dev.sigstore.http.ImmutableHttpParams;
+import dev.sigstore.oidc.client.ImmutableOidcToken;
 import dev.sigstore.oidc.client.OidcClient;
 import dev.sigstore.oidc.client.OidcException;
 import dev.sigstore.oidc.client.OidcToken;
 
+import java.io.IOException;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class JenkinsOidcClient implements OidcClient {
-    @Override
-    public boolean isEnabled(Map<String, String> map) {
-        return false;
+    private static final Logger log = Logger.getLogger(JenkinsOidcClient.class.getName());
+    private final String audience;
+    private final HttpParams httpParams;
+
+    public static Builder builder() {
+        return new Builder();
     }
 
-    @Override
-    public OidcToken getIDToken(Map<String, String> map) throws OidcException {
-        Map<String, String > env = System.getenv();
-        return null;
+    private JenkinsOidcClient(HttpParams httpParams, String audience) {
+        this.audience = audience;
+        this.httpParams = httpParams;
+    }
+
+    public boolean isEnabled(Map<String, String> env) {
+        String idToken = env.get("IDTOKEN");
+        if (idToken != null && !idToken.isEmpty()) {
+            return true;
+        } else {
+            log.info("Jenkins OIDC token not found: skipping OIDC token retrieval");
+            return false;
+        }
+    }
+
+    public OidcToken getIDToken(Map<String, String> env) throws OidcException {
+        String idToken = env.get("IDTOKEN");
+        if (idToken == null) {
+            throw new OidcException("Could not get Jenkins environment variable 'IDTOKEN'");
+        }
+
+        // Use the issuer URL as the base URL
+        String issuerUrl = "https://127.0.0.1:8082/oidc";
+        GenericUrl url = new GenericUrl(issuerUrl + "/some-endpoint?audience=" + this.audience);
+
+        try {
+            HttpRequest req = HttpClients.newRequestFactory(this.httpParams).buildGetRequest(url);
+            req.setParser(new GsonFactory().createJsonObjectParser());
+            req.getHeaders().setAuthorization("Bearer " + idToken);
+            req.getHeaders().setAccept("application/json; api-version=2.0");
+            req.getHeaders().setContentType("application/json");
+
+            JenkinsOidcJsonResponse resp = req.execute().parseAs(JenkinsOidcJsonResponse.class);
+            String tokenValue = resp.getValue();
+            JsonWebSignature jws = JsonWebSignature.parse(new GsonFactory(), tokenValue);
+
+            return ImmutableOidcToken.builder()
+                    .idToken(tokenValue)
+                    .issuer(jws.getPayload().getIssuer())
+                    .subjectAlternativeName(jws.getPayload().getSubject())
+                    .build();
+        } catch (IOException e) {
+            throw new OidcException("Could not obtain Jenkins OIDC token", e);
+        }
+    }
+
+    public static class JenkinsOidcJsonResponse {
+        private String value;
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
+
+    public static class Builder {
+        private HttpParams httpParams = ImmutableHttpParams.builder().build();
+        private String audience = "sigstore";
+
+        private Builder() {
+        }
+
+        public Builder audience(String audience) {
+            this.audience = audience;
+            return this;
+        }
+
+        public Builder httpParams(HttpParams httpParams) {
+            this.httpParams = httpParams;
+            return this;
+        }
+
+        public JenkinsOidcClient build() {
+            return new JenkinsOidcClient(this.httpParams, this.audience);
+        }
     }
 }
+
--- src/main/java/ce/kiran/Main.java
+++ src/main/java/ce/kiran/Main.java
@@ -1,17 +1,39 @@
 package ce.kiran;
 
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.Map;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.json.webtoken.JsonWebSignature;
+import dev.sigstore.KeylessSigner;
+import dev.sigstore.KeylessSignerException;
+import dev.sigstore.TrustedRootProvider;
+import dev.sigstore.bundle.Bundle;
+import dev.sigstore.encryption.signers.Signers;
+import dev.sigstore.fulcio.client.FulcioClient;
+import dev.sigstore.oidc.client.OidcClients;
+import dev.sigstore.rekor.client.RekorClient;
+import dev.sigstore.tuf.SigstoreTufClient;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Map<String, String> env = System.getenv();
-        String token = env.get("IDTOKEN");
-        JsonWebSignature jws = JsonWebSignature.parse(new GsonFactory(), token);
-        System.out.println(jws.getHeader());
+    public static void main(String[] args) throws IOException, InvalidAlgorithmParameterException, CertificateException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, KeylessSignerException {
+        OidcClients clients = OidcClients.of(JenkinsOidcClient.builder().build());
+        KeylessSigner signer = KeylessSigner.builder()
+                .oidcClients(clients)
+                .signer(Signers.newRsaSigner())
+                .trustedRootProvider(TrustedRootProvider.from(SigstoreTufClient.builder().usePublicGoodInstance()))
+                .fulcioUrl(FulcioClient.PUBLIC_GOOD_URI)
+                .rekorUrl(RekorClient.PUBLIC_GOOD_URI)
+                .minSigningCertificateLifetime(KeylessSigner.DEFAULT_MIN_SIGNING_CERTIFICATE_LIFETIME)
+                .build();
+        Bundle result = signer.signFile(Paths.get("src/main/java/ce/kiran/hello.txt"));
+        System.out.println(result.toJson());
     }
 }
